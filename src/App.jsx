ebe9a8cb39d8f485483c55a5b81d70d38ab84543
automationblog <sawin24@gmail.com>
2024-05-17 17:41:25 +0530

    cart page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,42 @@
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Menu from "./components/menu/menu.jsx";
 import Header from "./components/header/header.jsx";
 import Product from "./components/product/product.jsx";
 import Footer from "./components/footer/footer.jsx";
+import Cart from "./components/cart/cart.jsx";
 
 function App() {
   const [cart, setCart] = useState([]);
 
   return (
     <>
-      <Menu cart={cart} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/cart" element={<CartPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+function HomePage() {
+  return (
+    <>
+      <Menu />
       <Header />
-      <Product cart={cart} setCart={setCart} />
+      <Product />
       <Footer />
+    </>
+  );
+}
+
+function CartPage() {
+  return (
+    <>
+      <Menu />
+      <Cart />
     </>
   );
 }
